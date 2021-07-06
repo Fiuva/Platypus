9a421124a90c4e6822e65c021ffe0418adcacd9a
Fiuva <rgm745@gmail.com>
2021-07-06 16:26:57 +0200

    ,,,

--- antiSpam.js
+++ antiSpam.js
@@ -4,22 +4,25 @@ const antiSpam = () => {
 		warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
 		muteThreshold: 4, // Amount of messages sent in a row that will cause a mute
 		kickThreshold: 7, // Amount of messages sent in a row that will cause a kick.
-		banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
-		maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
+		banThreshold: 8, // Amount of messages sent in a row that will cause a ban.
+		maxInterval: 2500, // Amount of time (in milliseconds) in which messages are considered spam.
+		maxDuplicatesInterval: 2500, 
 		warnMessage: '{@user}, Por favor, deja el spam', // Message that will be sent in chat upon warning a user.
 		kickMessage: '**{user_tag}** ha sido expulsad@ por spamear.', // Message that will be sent in chat upon kicking a user.
 		muteMessage: '**{user_tag}** ha sido mutead@ por spamear.',// Message that will be sent in chat upon muting a user.
 		banMessage: '**{user_tag}** ha sido banead@ por spamear.', // Message that will be sent in chat upon banning a user.
-		maxDuplicatesWarning: 6, // Amount of duplicate messages that trigger a warning.
-		maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
-		maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
-		maxDuplicatesMute: 8, // Ammount of duplicate message that trigger a mute.
+		maxDuplicatesWarning: 3, // Amount of duplicate messages that trigger a warning.
+		maxDuplicatesKick: 5, // Amount of duplicate messages that trigger a warning.
+		maxDuplicatesBan: 7, // Amount of duplicate messages that trigger a warning.
+		maxDuplicatesMute: 10, // Ammount of duplicate message that trigger a mute.
 		ignoredPermissions: ['ADMINISTRATOR'], // Bypass users with any of these permissions.
 		ignoreBots: true, // Ignore bot messages.
 		verbose: true, // Extended Logs from module.
 		ignoredMembers: [], // Array of User IDs that get ignored.
 		muteRoleName: "Muted", // Name of the role that will be given to muted users!
-		removeMessages: true // If the bot should remove all the spam messages when taking action on a user!
+		removeMessages: true, // If the bot should remove all the spam messages when taking action on a user!
+		modLogsEnabled: true,
+		modLogsChannelName: "pruebas"
 		// And many more options... See the documentation.
 	});
 };
