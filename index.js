c93510f5b243ff8c3a05369bf0bbfbbd084ebd7e
Fiuva <rgm745@gmail.com>
2021-12-29 00:19:50 +0100

    adsbvasc

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ client.on('ready', async () => {
         const id = recDat[i].idDiscord
         const s = (await client.users.fetch(id)).presence.status
         const cs = (await client.users.fetch(id)).presence.clientStatus
-        const dispositivos = [];
+        var dispositivos = [];
         if(cs != null && cs != undefined) dispositivos = Object.keys(cs); 
         const date = new Date();
         if (s == 'online') {
