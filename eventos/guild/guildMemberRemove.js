314b2eedf1beb642815743f13a65537d8c46127e
Fiuva <rgm745@gmail.com>
2022-06-05 20:13:05 +0200

    fix guildMemberRemove

--- eventos/guild/guildMemberRemove.js
+++ eventos/guild/guildMemberRemove.js
@@ -3,16 +3,17 @@
 module.exports = async (client, member) => {
     if (member.guild.id != GUILD.SERVER_PLATY) return;
 
-    member.guild.channels.fetch("837367366227853423")
-        .then(channel => channel.setName('Ornitorrincos: ' + member.guild.memberCount));
+    member.guild.channels.cache.get("837367366227853423")
+        .channel.setName('Ornitorrincos: ' + member.guild.memberCount);
 
-    member.guild.channels.fetch(CANAL_TEXTO.BIENVENIDA)
-        .then(channel => channel.send(`${member.user} ha abandonado la familia de ornitorrincos :'<`));
+    member.guild.channels.cache.get(CANAL_TEXTO.BIENVENIDA)
+        .channel.send(`${member.user} ha abandonado la familia de ornitorrincos :'<`);
 
-    var recDat = await RecapData.find({ idDiscord: member.id })
-    if (recDat[0] != undefined) {
+    try { //mmm no lo he probado
         const mensajes = { total: 0, tiempos: [] }
-        await RecapData.findOneAndUpdate({ idDiscord: member.id }, { mensajes: mensajes, fechaMovil: null, fechaDnd: null, fechaOnline: null, fechaIdle: null }, { new: true });
+        await RecapData.findOneAndUpdate({ idDiscord: member.id }, { mensajes: mensajes, fechaMovil: null, fechaDnd: null, fechaOnline: null, fechaIdle: null });
         console.log(`Alguien salió, se modifica su documento de ${member.id}`)
+    } catch {
+        console.log("No existe el documento");
     }
 }
