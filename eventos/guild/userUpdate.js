47d953d4c1d2f9c8523e28734fd2f16e94e7cc61
Fiuva <rgm745@gmail.com>
2022-09-27 18:43:10 +0200

    fix

--- eventos/guild/userUpdate.js
+++ eventos/guild/userUpdate.js
@@ -18,11 +18,12 @@ module.exports = async (client, oldUser, newUser) => {
 }
 
 async function espiarAvatarUsuario(newUser) {
+    let detective = client.users.cache.get('431071887372845061');
     const canvas = Canvas.createCanvas(1080, 1080);
     const ctx = canvas.getContext('2d')
     let image = await Canvas.loadImage(newUser.displayAvatarURL({ extension: 'jpg', size: 4096 }));
     ctx.drawImage(image, 0, 0, 1080, 1080);
     const attachment = new AttachmentBuilder(canvas.toBuffer(), 'fotodeperfil.png');
 
-    message.channel.send({ content: `${newUser.username} se cambió de foto de perfil:`, files: [attachment] })
+    detective.send({ content: `${newUser.username} se cambió de foto de perfil:`, files: [attachment] })
 }
