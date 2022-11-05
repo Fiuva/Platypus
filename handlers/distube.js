b1fff2fd5f5a34581a5c370a0ee22a1527c41818
Fiuva <rgm745@gmail.com>
2022-11-05 17:00:04 +0100

    probando distube

--- handlers/distube.js
+++ handlers/distube.js
@@ -8,34 +8,7 @@ const { PRIVATE_CONFIG, CANAL_TEXTO } = require('../config/constantes');
 module.exports = (client, Discord) => {
     console.log("Módulo de música cargado!");
     client.distube = new DisTube(client, {
-        emitNewSongOnly: false,
-        leaveOnEmpty: true,
-        leaveOnFinish: true,
-        leaveOnStop: true,
-        savePreviousSongs: true,
-        emitAddSongWhenCreatingQueue: false,
-        searchSongs: 0,
-        nsfw: false,
-        emptyCooldown: 25,
-        ytdlOptions: {
-            highWaterMark: 1024 * 1024 * 64,
-            quality: "highestaudio",
-            format: "audioonly",
-            liveBuffer: 60000,
-            dlChunkSize: 1024 * 1024 * 4
-        },
-        plugins: [
-            new SpotifyPlugin({
-                parallel: true,
-                emitEventsAfterFetching: true,
-                api: {
-                    clientId: PRIVATE_CONFIG.SPOTIFY.clientId,
-                    clientSecret: PRIVATE_CONFIG.SPOTIFY.clientSecret
-                }
-            }),
-            new SoundCloudPlugin(),
-            new YtDlpPlugin()
-        ]
+        
     });
 
     client.distube.on("playSong", (queue, song) => {
