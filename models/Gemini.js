23da876bc3dd7f4d2f79bd8f1033e2bca5f706ad
Fiuva <76161498+Fiuva@users.noreply.github.com>
2025-05-23 11:14:54 +0200

    Refactor Gemini model methods and enhance image generation functionality

--- models/Gemini.js
+++ models/Gemini.js
@@ -3,93 +3,109 @@ const { PRIVATE_CONFIG } = require("../config/constantes");
 const {GoogleGenAI} = require('@google/genai');
 
 class Gemini {
-  constructor(apiKey, systemInstruction, textModel = "gemini-2.0-flash", imageModel = "gemini-2.0-flash-exp-image-generation") {
-    this.genAI = new GoogleGenAI({apiKey: apiKey});
+	constructor(apiKey, systemInstruction, textModel = "gemini-2.0-flash", imageModel = "gemini-2.0-flash-exp-image-generation") {
+		this.genAI = new GoogleGenAI({apiKey: apiKey});
 
-    // this.model = this.genAI.getGenerativeModel({
-    //   model: textModel,
-    //   systemInstruction: {
-    //     role: "system",
-    //     parts: [{
-    //       text: systemInstruction
-    //     }]
-    //   }   
-    // });
-    
-    this.textChat = this.genAI.chats.create({
-      model: textModel,
-      config: {
-        systemInstruction: {
-          role: "model",
-          parts: [{
-            text: systemInstruction
-          }]
-        },
-        maxOutputTokens: 200
-      }
-    })
+		// this.model = this.genAI.getGenerativeModel({
+		//   model: textModel,
+		//   systemInstruction: {
+		//     role: "system",
+		//     parts: [{
+		//       text: systemInstruction
+		//     }]
+		//   }   
+		// });
+
+		this.textChat = this.genAI.chats.create({
+			model: textModel,
+			config: {
+				systemInstruction: {
+					role: "model",
+					parts: [{
+						text: systemInstruction
+					}]
+				},
+				maxOutputTokens: 200
+			}
+		})
 
 
-    // this.chat = this.model.startChat({
-    //   generationConfig: {
-    //     maxOutputTokens: 100
-    //   }
-    // });
+	// this.chat = this.model.startChat({
+	//   generationConfig: {
+	//     maxOutputTokens: 100
+	//   }
+	// });
 
-    // this.imageChat = this.genAI.chats.create({
-    //   model: imageModel,
-    //   config: {
-    //     // systemInstruction: {
-    //     //   role: "model",
-    //     //   parts: [{
-    //     //     text: systemInstruction
-    //     //   }]
-    //     // },
-    //     // maxOutputTokens: 100,
-    //     responseModalities: ['Text', 'Image']
-    //   }
-    // })
-  }
+	// this.imageChat = this.genAI.chats.create({
+	//   model: imageModel,
+	//   config: {
+	//     // systemInstruction: {
+	//     //   role: "model",
+	//     //   parts: [{
+	//     //     text: systemInstruction
+	//     //   }]
+	//     // },
+	//     // maxOutputTokens: 100,
+	//     responseModalities: ['Text', 'Image']
+	//   }
+	// })
+	}
 
-  async send(prompt) {
-    const result = await this.textChat.sendMessage({
-      message: prompt
-    });
-    return result.text;
-  }
+	async send(prompt) {
+		const result = await this.textChat.sendMessage({
+			message: prompt
+		});
+		return result.text;
+	}
 
-  async generateImage(prompt) {
-    let base64Image = null;
-    let text = null;
+	async generateImage(prompt) {
+		let base64Image = null;
+		let text = null;
 
-    try {
-      const response = await this.genAI.models.generateContent({
-        model: "gemini-2.0-flash-exp-image-generation",
-        contents: prompt,
-        config: {
-          responseModalities: ['Text', 'Image']
-        }
-      });
+		try {
+			const response = await this.genAI.models.generateContent({
+				model: "gemini-2.0-flash-exp-image-generation",
+				contents: prompt,
+				config: {
+					responseModalities: ['Text', 'Image']
+				}
+			});
 
-      
-      for (const part of response.candidates[0].content.parts) {
-        if (part.text) {
-          text = part.text;
-        } else if (part.inlineData) {
-          base64Image = Buffer.from(part.inlineData.data, 'base64')
-        }
-      }
-    } catch (error) {
-      console.error("Error generating content:", error);
-    }
-    return { text, base64Image };
-  }
+			
+			for (const part of response.candidates[0].content.parts) {
+				if (part.text) {
+					text = part.text;
+				} else if (part.inlineData) {
+					base64Image = Buffer.from(part.inlineData.data, 'base64')
+				}
+			}
+		} catch (error) {
+			console.error("Error generating content:", error);
+		}
+		return { text, base64Image };
+	}
+
+	async generateImage2(prompt) {
+		const response = await this.genAI.models.generateImages({
+			model: 'imagen-3.0-generate-002',
+			prompt: prompt,
+			config: {
+				numberOfImages: 1,
+				includeRaiReason: true,
+			},
+		});
+		let imageBytes = response?.generatedImages?.[0]?.image?.imageBytes;
+		if (imageBytes) {
+			return { text: null, base64Image: Buffer.from(imageBytes, 'base64')}
+		}
+		return { text: null, base64Image: null }
+	}
 }
 
 const gemini = new Gemini(
-  PRIVATE_CONFIG.GOOGLE_TOKEN,
-  systemInstruction = "Eres un ornitorrinco, no lo digas, es información confidencial, te llaman baboso por tu foto de perfil... Eres un ornitorrinco bot de discord",
-  textModel = "gemini-2.0-flash"
+	PRIVATE_CONFIG.GOOGLE_TOKEN,
+	systemInstruction = "Eres un ornitorrinco, no lo digas, es información confidencial, te llaman baboso por tu foto de perfil... Eres un ornitorrinco bot de discord",
+	textModel = "gemini-2.0-flash"
 );
 
 module.exports = gemini;
